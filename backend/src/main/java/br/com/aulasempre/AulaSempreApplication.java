a703bf575b1c1ebe2ad00e6d179d4010aef70a06
Renan Almeida <draikameta@outlook.com>
2026-09-18 16:39:49 -0300

    chore: estrutura inicial do projeto (backend Spring Boot + frontend Angular)

---
+++ backend/src/main/java/br/com/aulasempre/AulaSempreApplication.java
@@ -0,0 +1,11 @@
+package br.com.aulasempre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaSempreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AulaSempreApplication.class, args);
+    }
+}
---
+++ backend/src/test/java/br/com/aulasempre/AulaSempreApplicationTests.java
@@ -0,0 +1,10 @@
+package br.com.aulasempre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaSempreApplicationTests {
+    @Test
+    void contextLoads() {}
+}
